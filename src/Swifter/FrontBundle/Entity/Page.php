dc2e534e764f3f373210dd948c665299a9117dd7
Taras Velykyy <tvelykyy@llnw.com>
2013-12-26 17:45:07 +0200

    Creating Page Entity.

---
+++ src/Swifter/FrontBundle/Entity/Page.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Swifter\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="page")
+ */
+class Page
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    protected $parent;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    protected $uri;
+
+    protected $template;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+
+    public function getUri()
+    {
+        return $this->uri;
+    }
+}
