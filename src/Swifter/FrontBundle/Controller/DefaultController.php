1d8a1870310447682e9ae629f8882b6f8c8b891e
Taras Velykyy <tvelykyy@llnw.com>
2013-12-27 11:46:48 +0200

    Skeleton page rendering.

--- src/Swifter/FrontBundle/Controller/DefaultController.php
+++ src/Swifter/FrontBundle/Controller/DefaultController.php
@@ -6,8 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($route)
+    public function indexAction($uri)
     {
-        return $this->render('SwifterFrontBundle:Default:index.html.twig', array('name' => $route));
+        $fullUri = $this->leadWithSlash($uri);
+        $page = $this->getDoctrine()
+            ->getRepository('SwifterFrontBundle:Page')
+            ->findOneByUri($fullUri);
+
+        if (!$page) {
+            throw $this->createNotFoundException('Page not found.');
+        }
+
+        return $this->render('SwifterFrontBundle:Default:index.html.twig', array('name' => $fullUri));
+    }
+
+    private function leadWithSlash($uri)
+    {
+        return '/'.$uri;
     }
 }
