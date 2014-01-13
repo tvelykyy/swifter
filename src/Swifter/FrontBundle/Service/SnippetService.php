0a6c3f661e2c69fa4df379fea86fb3ccff5d8d4a
Taras Velykyy <tavelyky@gmail.com>
2014-01-13 22:41:05 +0200

    Retrieving snippets from PageBlock content.

--- src/Swifter/FrontBundle/Service/SnippetService.php
+++ src/Swifter/FrontBundle/Service/SnippetService.php
@@ -18,10 +18,22 @@ class SnippetService
     private function resolveSnippetsForPageBlock(PageBlock $pageBlock)
     {
         $snippets = $this->getSnippetsFromPageBlockContent($pageBlock);
+
     }
 
-    private function getSnippertsFromPageBlockContent(PageBlock $pageBlock)
+    private function getSnippetsFromPageBlockContent(PageBlock $pageBlock)
     {
+        $matchedSnippets = null;
+        preg_match_all('/\\[\\[(.*?)\\]\\]/', $pageBlock->getContent(), $matchedSnippets);
 
+        $foundSnippets = array();
+
+        /* $snippets[0] returns match like this [[(.*?)]], we need (.*?)., so we take [1] */
+        foreach ($matchedSnippets[1] as $matchedSnippet)
+        {
+            $foundSnippets[] = $matchedSnippet;
+        }
+
+        return $foundSnippets;
     }
 }
