389a10b6310e4bee5637ecc4e394e3b287b38ff7
Taras Velykyy <tvelykyy@llnw.com>
2014-01-14 12:32:03 +0200

    Implementing SnippetService to call service from snippet config.

--- src/Swifter/FrontBundle/Service/SnippetService.php
+++ src/Swifter/FrontBundle/Service/SnippetService.php
@@ -4,9 +4,19 @@ namespace Swifter\FrontBundle\Service;
 
 use Swifter\FrontBundle\Entity\Page;
 use Swifter\FrontBundle\Entity\PageBlock;
+use Swifter\FrontBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
 class SnippetService
 {
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
     public function resolveSnippetsForPage(Page $page)
     {
         foreach($page->getPageBlocks()->toArray() as $pageBlock)
@@ -45,9 +55,13 @@ class SnippetService
     {
         $tokens = explode('?', $snippetSource);
 
-        $executionPart = explode('.', $tokens[0]);
-        $service = new $executionPart[0];
-        $method = $executionPart[1];
+        $executionPart = explode('#', $tokens[0]);
+        $serviceName = $executionPart[0];
+        $methodName = $executionPart[1];
+
+        $service = $this->container->get($serviceName);
+
+        $result = call_user_func_array(array($service, $methodName), array());
 
         $templateId = $executionPart[2];
     }
