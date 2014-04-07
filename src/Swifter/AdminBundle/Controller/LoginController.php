9ac6c65777d21418f66705d29806124fedab8665
Taras Velykyy <tavelyky@gmail.com>
2014-04-07 13:12:27 +0300

    #4 login page redirects to landing if user logged in.

--- src/Swifter/AdminBundle/Controller/LoginController.php
+++ src/Swifter/AdminBundle/Controller/LoginController.php
@@ -8,6 +8,9 @@ class LoginController extends Controller
 {
     public function renderLoginPageAction()
     {
+        if( $this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED') ){
+            return $this->redirect($this->generateUrl('admin_landing_page'));
+        }
         return $this->render('SwifterAdminBundle::login.html.twig');
     }
 }
