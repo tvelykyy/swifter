3f7c86174ee21ce664d7c508728bb8294b4dc7d9
Taras Velykyy <tavelyky@gmail.com>
2014-04-13 00:34:45 +0300

    Working with functional tests for LoginController.

--- src/Swifter/AdminBundle/Tests/LoginControllerTest.php
+++ src/Swifter/AdminBundle/Tests/LoginControllerTest.php
@@ -20,5 +20,25 @@ class LoginControllerTest extends WebTestCase
 
         /* Then. */
         $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertEquals('Login', $crawler->filter('button[type=submit]')->first()->text());
+        $this->assertNotEmpty($crawler->filter('input[type=hidden][name=_csrf_security_token]')->first()->attr('value'));
+    }
+
+    public function testShouldRedirectToLandingPageIfRequestingLoginPageBeingAuthenticated()
+    {
+        /* Given. */
+        $crawler = $this->client->request('GET', 'admin/login');
+        $loginButton = $crawler->selectButton('Login');
+        $form = $loginButton->form(array(
+            'email'     => 'admin@m.com',
+            'password'  => 'admin',
+        ));
+        $this->client->submit($form);
+
+        /* When. */
+        $crawler = $this->client->request('GET', 'admin/login');
+
+        /* Then. */
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
     }
 }
