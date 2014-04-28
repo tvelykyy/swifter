e20b5085c6b5e4b643d71b079df77f4a07ff3f90
Taras Velykyy <tvelykyy@llnw.com>
2014-04-28 14:04:07 +0300

    Added test to PagesController.

---
+++ src/Swifter/AdminBundle/Tests/Controller/PagesControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Swifter\AdminBundle\Tests\Controller;
+
+class PagesControllerTest extends ControllerTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->authenticateAsAdmin();
+    }
+
+    public function testShouldReturnAllPagesInJson()
+    {
+        /* When. */
+        $pages = $this->retrievePages();
+
+        /* Then. */
+        $this->assertEquals(200, $this->getResponse()->getStatusCode());
+        $this->assertEquals('application/json', $this->getResponse()->headers->get('Content-Type'));
+        $this->assertEquals(3, sizeof($pages));
+        $this->assertEquals('Main', $pages[0]->name);
+        $this->assertEquals('/', $pages[0]->uri);
+        $this->assertEquals('Main Template', $pages[0]->template->title);
+
+        $this->assertEquals('News', $pages[1]->name);
+        $this->assertEquals('/news', $pages[1]->uri);
+        $this->assertEquals('Main Template', $pages[1]->template->title);
+
+        $this->assertEquals('First News', $pages[2]->name);
+        $this->assertEquals('/news/first', $pages[2]->uri);
+        $this->assertEquals('Main Template', $pages[2]->template->title);
+    }
+
+    protected function retrievePages()
+    {
+        $this->client->request('GET', $this->generateRoute('admin_retrieve_pages'));
+        $blocks = json_decode($this->getResponse()->getContent());
+
+        return $blocks;
+    }
+
+}
